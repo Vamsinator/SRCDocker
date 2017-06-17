448bf582cd23dcdd4d1c4fc3a9609646aea33739
XLOCKDOWN <saurabhsogi@gmail.com>
2017-06-16 23:01:23 -0500

    M

--- startup.bash
+++ startup.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 . /home/docker/ws/install/setup.bash
-command python -m SimpleHTTPServer 8000 &
+#command python -m SimpleHTTPServer 8000 &
 command roscore -p 11311 &
 python ./Funstuff/serverSide.py && ./Funstuff/sender.py && ./dockerTest/serverSide.py
